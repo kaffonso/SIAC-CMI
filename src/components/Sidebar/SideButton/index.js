cb6103ba5b19ad8f14694ed6a507683a5ff57137
Kenny Afonso <kafonso.dev@gmail.com>
2022-05-24 12:01:51 -0100

    commented the codes

--- src/components/Sidebar/SideButton/index.js
+++ src/components/Sidebar/SideButton/index.js
@@ -5,17 +5,16 @@ import "./styles.css";
 export default function SideButton(props) {
   const [status, setStatus] = useState(props.status);
 
-  const link = props.url.concat(props.title)
-  console.log(link)
+  const link = props.url.concat(props.title) // receber as duas partes do url e concatenar para termos o link ou path para a respetiva pagina
 
-  const nav = useNavigate();
+  const nav = useNavigate(); //componente que lida com os links, se inicia assim
 
   return (
     <div className="button">
       <button
         className={status}
         onClick={() => {
-          nav(link);
+          nav(link); // ao clicar sera redirecionado para o url ou link respetivo 
         }}
       >
         {props.name}
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,10 +5,10 @@ import logo from "../../img/logo.png";
 import "./styles.css";
 
 export default function Sidebar(props) {
-  const userType = props.data.type;
-  let url;
+  const userType = props.data.type; //receber o tipo de usuario, que vem do json
+  let url; //variavel para guardar o url
 
-  if (userType === "Funcionario") {
+  if (userType === "Funcionario") { //comparar o tipo de usuario com os 3 posiveis tipos, atribuir um path para o respetivo
     url = "/funcionario/";
   } else if (userType === "Aluno") {
     url = "/aluno/";
@@ -26,8 +26,8 @@ export default function Sidebar(props) {
           <SideButton
             name="CANDIDATURA"
             status="active"
-            url={url}
-            title="candidatura"
+            url={url} //passar o url como prop para o butao
+            title="candidatura" // a segund parte de url, que e o titulo do butao
           />
           <SideButton
             name="MATRICULA"
