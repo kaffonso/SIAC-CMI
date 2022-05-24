e8459d8319052f30f59e25ffc8cf5ac015729437
Kenny Afonso <kafonso.dev@gmail.com>
2022-05-24 15:45:28 -0100

    added matricula and candidatura pages on candidato

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
-import Candidatura from "./pages/Funcionario/Candidatura";
-import Matricula from "./pages/Funcionario/Matricula";
+import CandidaturaF from "./pages/Funcionario/Candidatura";
+import MatriculaF from "./pages/Funcionario/Matricula";
+
+import CandidaturaC from "./pages/Candidato/Candidatura";
 import ErrorPage from "./pages/ErrorPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/funcionario/candidatura" element={<Candidatura />}/> 
-        <Route path="/funcionario/matricula" element={<Matricula />}/>
+        <Route path="/funcionario/candidatura" element={<CandidaturaF />}/> 
+        <Route path="/funcionario/matricula" element={<MatriculaF />}/>
+        <Route path="/candidato/candidatura" element={<CandidaturaC />}/>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
