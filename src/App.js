835b4943de69e33799d91a941aaaae06e60e3efe
Kenny Afonso <kafonso.dev@gmail.com>
2022-05-24 14:31:44 -0100

    added matricula route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 import Candidatura from "./pages/Funcionario/Candidatura";
+import Matricula from "./pages/Funcionario/Matricula";
 import ErrorPage from "./pages/ErrorPage";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/funcionario/candidatura" element={<Candidatura />}/> 
+        <Route path="/funcionario/matricula" element={<Matricula />}/>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
