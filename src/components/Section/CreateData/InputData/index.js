47b471057b4ffca76093e228b79eda7ebcb5b6b5
Kenny Afonso <kafonso.dev@gmail.com>
2022-05-25 19:30:58 -0100

    can get data from form on candidatura

--- src/components/Section/CreateData/InputData/index.js
+++ src/components/Section/CreateData/InputData/index.js
@@ -1,9 +1,14 @@
-import './styles.css'
-export default function InputData(props){
-  return(
+import "./styles.css";
+export default function InputData(props) {
+  return (
     <div className="input_data">
       <label>{props.name}</label>
-      <input type={props.type} value={props.value}/>
+      <input
+        type={props.type}
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={props.change}
+      />
     </div>
-  )
+  );
 }
--- src/components/Section/CreateData/index.js
+++ src/components/Section/CreateData/index.js
@@ -1,35 +1,121 @@
 import InputData from "./InputData";
+import { useState } from "react";
 import "./styles.css";
 
 export default function CreateData() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [CNI, setCNI] = useState("");
+  const [email, setEmail] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [sex, setSex] = useState("");
+  const [islandAcronym, setIslandAcronym] = useState("");
+  const [courseAcronym, setCourseAcronym] = useState("");
+  const [courseName, setCourseName] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const courseData = {
+      "courseAcronym": courseAcronym,
+    };
+
+    const islandData ={
+      "islandAcronym": islandAcronym
+    }
+
+    const candidatura = {
+      "code": 201231,
+      "name": name,
+      "nasc": date,
+      "cni": CNI,
+      "telephone": telephone,
+      "email": email,
+      "course": courseData,
+      "sex": sex,
+      "island": islandData
+    };
+
+    console.log(candidatura);
+
+    setName('')
+    setDate('')
+    setCNI('')
+    setEmail('')
+    setSex('')
+    setTelephone('')
+    setCourseAcronym('')
+    setCourseName('')
+    
+    e.target.reset();
+  };
+
   return (
     <div className="data_container">
       <div className="data_form">
-        <form method="post" id="data_form_input">
+        <form method="post" id="data_form_input" onSubmit={handleSubmit}>
           <p>Informacoes Pessoais</p>
           <div className="data_personal">
-            <InputData type="text" name="Nome" />
-            <InputData type="date" name="Nascimento" />
-            <InputData type="text" name="CNI" />
-            <InputData type="email" name="Email" />
-            <InputData type="number" name="Telefone" />
+            <InputData
+              type="text"
+              name="Nome"
+              placeholder="Nome"
+              id="name"
+              change={(e) => setName(e.target.value)}
+            />
+            <InputData
+              type="date"
+              name="Nascimento"
+              id="nasc"
+              change={(e) => setDate(e.target.value)}
+            />
+            <InputData
+              type="text"
+              name="CNI"
+              id="cni"
+              change={(e) => setCNI(e.target.value)}
+            />
+            <InputData
+              type="email"
+              name="Email"
+              id="email"
+              change={(e) => setEmail(e.target.value)}
+            />
+            <InputData
+              type="number"
+              name="Telefone"
+              id="telephone"
+              change={(e) => setTelephone(e.target.value)}
+            />
             <div className="dropdown">
               <label for="sex">Sexo</label>
-              <select id="sex_element" name="sex">
-                <option value="SA">Masculino</option>
-                <option value="SV">Feminino</option>
+              <select
+                id="sex_element"
+                name="sex"
+                onChange={(e) => setSex(e.target.value)}
+              >
+                <option value="none" selected="disabled" hidden>
+                  Escolha o Sexo
+                </option>
+                <option value="M">Masculino</option>
+                <option value="F">Feminino</option>
               </select>
             </div>
             <div className="data_course">
               <div className="dropdown">
                 <label for="island">Ilha de Residencia</label>
-                <select id="island_element" name="island">
+                <select
+                  id="island_element"
+                  name="island"
+                  onChange={(e) => setIslandAcronym(e.target.value)}
+                >
+                  <option value="none" selected="disabled" hidden>
+                    Escolha a Ilha
+                  </option>
                   <option value="SA">Santo Antao</option>
                   <option value="SV">Sao Vicente</option>
-                  <option value="SN">Sao Nicolau</option>
-                  <option value="SL">Sal</option>
-                  <option value="BV">Boavista</option>
-                  <option value="MA">Maio</option>
+                  <option value="SN">Sao Nicolau</option>setSex
                   <option value="ST">Santiago</option>
                   <option value="FG">Fogo</option>
                   <option value="BR">Brava</option>
@@ -50,7 +136,17 @@ export default function CreateData() {
           <div className="data_course">
             <div className="dropdown">
               <label for="course">Curso</label>
-              <select id="course_element" name="course">
+              <select
+                id="course_element"
+                name="course"
+                defaultValue=""
+                onChange={(e) => {
+                  setCourseAcronym(e.target.value);
+                }}
+              >
+                <option value="none" selected="disabled" hidden>
+                  Escolha o Curso
+                </option>
                 <option value="LEIT">
                   Licencietura Engenharia Informatica e Telecomunicacoes
                 </option>
