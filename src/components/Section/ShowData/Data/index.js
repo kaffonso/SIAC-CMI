3b5415aa2d2c0ab507a1afe8bf95f8d6d7f4f619
Angelo-Bruno <brunoangelo.dev@gmail.com>
2022-05-25 09:52:47 -0100

    fetch data from json-server

--- src/components/Section/ShowData/Data/index.js
+++ src/components/Section/ShowData/Data/index.js
@@ -1,38 +1,50 @@
 import "../styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Data(props) {
-  const data = props.data.info
-  const [searchName, setSearchName] = useState("");
+  //const data = props.data.info
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState("");
 
-  const filterReturn = (filter, value) => {
-    if (filter === "") {
-      return value;
-    } else if (value.name.toLowerCase().includes(filter.toLowerCase())) {
-      return value;
-    }
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  const fetchAllData = async () => {
+    return await axios
+      .get("http://localhost:3333/info")
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  };
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    return await axios
+      .get(`http://localhost:3333/info?q=${value}`)
+      .then((response) =>{
+         setData(response.data)
+         setValue("")
+      })
+      .catch((err) => console.log(err));
   };
 
-  data.sort((a,b) =>(a.code - b.code)) // ordenar dados por codigo, ordem crescente
+  data.sort((a, b) => a.code - b.code); // ordenar dados por codigo, ordem crescente
 
-  const data_student = data
-    .filter((value) => filterReturn(searchName, value))
-    .map((data) => {
-      const space = "";
-      const name = space.concat(data.name, "").toUpperCase();
-      const status = space.concat(data.status, "").toUpperCase();
-      const island = space.concat(data.island.islandName, "").toUpperCase();
-
-      return (
-        <div className="data_element" key={data.code}>
-          <p className="box">{data.code}</p>
-          <p className="box">{name}</p>
-          <p className="box">{data.course.courseAcronym}</p>
-          <p className="box">{data.sex}</p>
-          <p className="box">{island}</p>
-          <p className="box">{status}</p>
-        </div>
-      );
-    });
+  const data_student = data.map((data) => {
+    const space = "";
+    const name = space.concat(data.name, "").toUpperCase();
+    const status = space.concat(data.status, "").toUpperCase();
+    const island = space.concat(data.island.islandName, "").toUpperCase();
+    return (
+      <div className="data_element" key={data.code}>
+        <p className="box">{data.code}</p>
+        <p className="box">{name}</p>
+        <p className="box">{data.course.courseAcronym}</p>
+        <p className="box">{data.sex}</p>
+        <p className="box">{island}</p>
+        <p className="box">{status}</p>
+      </div>
+    );
+  });
   return (
     <>
       <div className="data_searchbar">
@@ -41,12 +53,12 @@ export default function Data(props) {
           type="text"
           id="searchbar_input"
           placeholder="Procurar"
-          onChange={(event) => {
-            setSearchName(event.target.value);
-          }}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
+        <input type="submit" onClick={handleSearch} value="Search"/>
       </div>
-      
+
       <div className="data_header">
         <p className="box">CODIGO</p>
         <p className="box">NOME</p>
@@ -56,9 +68,7 @@ export default function Data(props) {
         <p className="box">ESTADO</p>
       </div>
 
-      <div className="data_wrapper">
-        {data_student}
-      </div>
+      <div className="data_wrapper">{data_student}</div>
     </>
   );
 }
