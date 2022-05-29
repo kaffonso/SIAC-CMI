d12a34abced79bdeaffbfc4359238ab326994bb7
Kenny Afonso <kafonso.dev@gmail.com>
2022-05-29 17:18:26 -0100

    capitalize title on submit button

--- src/components/Section/SubmitButton/index.js
+++ src/components/Section/SubmitButton/index.js
@@ -1,9 +1,12 @@
 import './styles.css'
 
 export default function SubmitButton(props) {
+  const space = "";
+  const title = space.concat(props.title, "").toUpperCase();
+
   return (
     <div className="btn_submit">
-      <input type="submit" value={props.title} />
+      <input type="submit" value={title} />
     </div>
   );
 }
