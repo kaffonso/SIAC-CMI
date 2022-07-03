2bc624a42219d4f91995178276f7a92eb6047053
Angelo-Bruno <brunoangelo.dev@gmail.com>
2022-07-03 13:23:52 -0100

    candidature filter  not functional|need: add by code filter

--- src/api/src/controllers/RegistrationController.js
+++ src/api/src/controllers/RegistrationController.js
@@ -1,4 +1,59 @@
 const pool = require("../config/database");
+const filter = (query) => {
+  const { name, sex } = query
+
+  switch (true) {
+      case name && !sex:
+        query = `SELECT registration.id, registration.created_at, row_to_json(student) as student, row_to_json(course) as course,
+        row_to_json(user_info) as user FROM public.registration` + 
+        ` inner join (select * from public.student) student on student.code = public.registration.id_student` + 
+        ` inner join (select * from public.course) course on course.id = public.registration.id_course`+
+        ` INNER JOIN (SELECT * FROM public.user) user_info ON student.id_user = user_info.id
+          WHERE student.full_name like '${name}%'`
+          break;
+
+
+      case sex && !name:
+        query = `SELECT registration.id, registration.created_at, row_to_json(student) as student, row_to_json(course) as course,
+        row_to_json(user_info) as user FROM public.registration` + 
+        ` inner join (select * from public.student) student on student.code = public.registration.id_student` + 
+        ` inner join (select * from public.course) course on course.id = public.registration.id_course`+
+        ` INNER JOIN (SELECT * FROM public.user) user_info ON student.id_user = user_info.id  
+          WHERE user_info.sex = '${sex.toUpperCase()}'`
+          break;
+
+  
+
+      case name && sex :
+        query = `SELECT registration.id, registration.created_at, row_to_json(student) as student, row_to_json(course) as course,
+        row_to_json(user_info) as user FROM public.registration` + 
+        ` inner join (select * from public.student) student on student.code = public.registration.id_student` + 
+        ` inner join (select * from public.course) course on course.id = public.registration.id_course`+
+        ` INNER JOIN (SELECT * FROM public.user) user_info ON student.id_user = user_info.id 
+          WHERE student.full_name = '${name}' AND user.sex = '${sex.toUpperCase()}'`
+          break;
+
+     
+
+      default:
+        query = `SELECT registration.id, registration.created_at, row_to_json(student) as student, row_to_json(course) as course,
+        row_to_json(user_info) as user FROM public.registration` + 
+        ` inner join (select * from public.student) student on student.code = public.registration.id_student` + 
+        ` inner join (select * from public.course) course on course.id = public.registration.id_course`+
+        ` INNER JOIN (SELECT * FROM public.user) user_info ON student.id_user = user_info.id `
+
+      
+          // query = `SELECT student.code, student.full_name, row_to_json(address) as address, 
+          // row_to_json(user_info) as user, row_to_json(course) as course FROM public.student INNER`+
+          // ` JOIN (SELECT * FROM public.address) address ON public.student.id_address = address.id`+
+          // ` INNER JOIN (SELECT * FROM public.user) user_info ON public.student.id_user = user_info.id `+
+          // ` INNER JOIN (SELECT * FROM public.course) course ON course.id = public.student.id_course`
+          // break;
+
+        
+  }
+  return query;
+}
 
 module.exports = {
   async read(req, res, next) {
@@ -8,15 +63,13 @@ module.exports = {
   },
 
   async read_full(req, res, next) {
-    pool
-      .query(
-        `SELECT registration.id, registration.created_at, row_to_json(student) as student, row_to_json(course) as course FROM public.registration` + 
-        ` inner join (select * from public.student) student on student.code = public.registration.id_student` + 
-        ` inner join (select * from public.course) course on course.id = public.registration.id_course`
-      )
-      .then((dados) => {
-        res.json(dados.rows);
-      })
+
+      const query = filter(req.query)
+      const {rows} = await pool.query(query)
+  
+      res.json(rows)
+  
+    
   },
 
   async store(req, res, next) {
